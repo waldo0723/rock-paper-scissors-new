fb6a7cba60cfff9d02d262a571042070a9a831cd
waldo0723 <63370364+waldo0723@users.noreply.github.com>
2020-08-21 15:13:10 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ $(".paperbut").click(function() {
     $(".pick").show();
     $(".paperchoice").show();
     $(".score").hide();
+    $(".imagepen").hide();
+    $(".spockbut").hide();
+    $(".lizardbut").hide();
 
 
     var score = $(".score").text();
@@ -25,7 +28,11 @@ $(".paperbut").click(function() {
 
     console.log('hh' + div1)
 
-
+    if(window.innerWidth < 575 )
+    {
+      $(".paperchoice").toggleClass("btn");
+      $(".d2").toggleClass("d");
+    }
 
     if(window.innerWidth < 768)
   {
@@ -52,7 +59,7 @@ $(".paperbut").click(function() {
   }
 
 
-  var results  =   won(div);
+  var results  =   wonpaper(div);
 
     $(".headyou").delay(500).fadeIn();
 
@@ -95,6 +102,9 @@ $(".rockbut").click(function() {
     $(".pick").show();
     $(".rockchoice").show();
     $(".score").hide();
+    $(".imagepen").hide();
+    $(".spockbut").hide();
+    $(".lizardbut").hide();
 
 
     var score = $(".score").text();
@@ -114,6 +124,11 @@ $(".rockbut").click(function() {
 
     console.log('hh' + div1)
 
+    if(window.innerWidth < 575 )
+    {
+      $(".rockchoice").toggleClass("btn");
+      $(".d2").toggleClass("d");
+    }
 
 
     if(window.innerWidth < 768)
@@ -184,7 +199,9 @@ $(".sicbut").click(function() {
     $(".pick").show();
     $(".scissorschoice").show();
     $(".score").hide();
-
+    $(".imagepen").hide();
+    $(".spockbut").hide();
+    $(".lizardbut").hide();
 
     var score = $(".score").text();
 
@@ -203,7 +220,11 @@ $(".sicbut").click(function() {
 
     console.log('hh' + div1)
 
-
+    if(window.innerWidth < 575 )
+    {
+      $(".scissorschoice").toggleClass("btn");
+      $(".d2").toggleClass("d");
+    }
 
     if(window.innerWidth < 768)
   {
@@ -265,6 +286,198 @@ else{
   }
 
 )
+$(".spockbut").click(function() {
+    $(".img-fluid").hide();
+    $(".paperbut").hide();
+    $(".sicbut").hide();
+    $(".rockbut").hide();
+    $(".pick").show();
+    $(".spockchoice").show();
+    $(".score").hide();
+    $(".imagepen").hide();
+    $(".spockbut").hide();
+    $(".lizardbut").hide();
+
+    var score = $(".score").text();
+
+    var house = showHouse();
+
+    console.log(house);
+
+
+    var div = "." + house + "house";
+
+    console.log('here' + div);
+
+    $(div).delay(2000);
+
+    var div1 = $("." + house + "house");
+
+    console.log('hh' + div1)
+
+
+    if(window.innerWidth < 575 )
+    {
+      $(".spockchoice").toggleClass("btn");
+      $(".d2").toggleClass("d");
+    }
+
+
+    if(window.innerWidth < 768)
+  {
+    div1.animate({
+      height: '170px',
+      width: '170px'
+    }, "slow");
+
+    div1.animate({
+      height: '150px',
+      width: '150px'
+    }, "slow");
+  }
+  else{
+    div1.animate({
+      height: '220px',
+      width: '220px'
+    }, "slow");
+
+    div1.animate({
+      height: '200px',
+      width: '200px'
+    }, "slow");
+  }
+
+
+  var results  =   wonspock(div);
+
+    $(".headyou").delay(500).fadeIn();
+
+    $(".headhouse").delay(500).fadeIn();
+
+var you = 0;
+if(results === "1")
+{
+  you ="1";
+  win = "YOU WIN"
+  score = parseInt(score) + parseInt(you);
+
+}
+else if(results === "-1")
+{
+  win= "YOU LOSE"
+  you = "1";
+  score = parseInt(score) - parseInt(you);
+}
+else{
+  win = "DRAW"
+}
+    $(".score").text(score);
+    $(".win").text(win);
+
+
+    $(".win").delay(3500).fadeIn();
+    $(".play").delay(3500).fadeIn();
+    $(".score").delay(3500).fadeIn();
+    console.log(results);
+
+  }
+
+)
+$(".lizardbut").click(function() {
+    $(".img-fluid").hide();
+    $(".paperbut").hide();
+    $(".sicbut").hide();
+    $(".rockbut").hide();
+    $(".pick").show();
+    $(".lizardchoice").show();
+    $(".score").hide();
+    $(".imagepen").hide();
+    $(".spockbut").hide();
+    $(".lizardbut").hide();
+
+    var score = $(".score").text();
+
+    var house = showHouse();
+
+    console.log(house);
+
+
+    var div = "." + house + "house";
+
+    console.log('here' + div);
+
+    $(div).delay(2000);
+
+    var div1 = $("." + house + "house");
+
+    console.log('hh' + div1)
+
+    if(window.innerWidth < 575 )
+    {
+      $(".lizardchoice").toggleClass("btn");
+      $(".d2").toggleClass("d");
+    }
+
+    if(window.innerWidth < 768)
+  {
+    div1.animate({
+      height: '170px',
+      width: '170px'
+    }, "slow");
+
+    div1.animate({
+      height: '150px',
+      width: '150px'
+    }, "slow");
+  }
+  else{
+    div1.animate({
+      height: '220px',
+      width: '220px'
+    }, "slow");
+
+    div1.animate({
+      height: '200px',
+      width: '200px'
+    }, "slow");
+  }
+
+
+  var results  =   wonlizard(div);
+
+    $(".headyou").delay(500).fadeIn();
+
+    $(".headhouse").delay(500).fadeIn();
+
+var you = 0;
+if(results === "1")
+{
+  you ="1";
+  win = "YOU WIN"
+  score = parseInt(score) + parseInt(you);
+
+}
+else if(results === "-1")
+{
+  win= "YOU LOSE"
+  you = "1";
+  score = parseInt(score) - parseInt(you);
+}
+else{
+  win = "DRAW"
+}
+    $(".score").text(score);
+    $(".win").text(win);
+
+
+    $(".win").delay(3500).fadeIn();
+    $(".play").delay(3500).fadeIn();
+    $(".score").delay(3500).fadeIn();
+    console.log(results);
+
+  }
+
+)
 
 $(".play").click(function() {
 
@@ -276,28 +489,40 @@ $(".play").click(function() {
   $(".dothouse3").hide();
   $(".play").hide();
   $(".win").hide();
-  $(".img-fluid").show();
+  $(".imagepen").show();
   $(".paperbut").show();
   $(".sicbut").show();
   $(".rockbut").show();
   $(".scissorshouse").hide();
   $(".paperhouse").hide();
   $(".rockhouse").hide();
+  $(".lizardhouse").hide();
+  $(".spockhouse").hide();
   $(".pick").hide();
   $(".dot1").hide();
   $(".paperchoice").hide();
   $(".rockchoice").hide();
   $(".scissorschoice").hide();
+  $(".spockchoice").hide();
+  $(".lizardchoice").hide();
+  $(".spockbut").show();
+  $(".lizardbut").show();
+  if(window.innerWidth < 575 )
+  {
+    $(".paperchoice").toggleClass("btn");
+    $(".d2").toggleClass("d");
+  }
+
 
 })
 
 function showHouse() {
 
-  let options = ['rock', 'paper', 'scissors'];
+  let options = ['rock', 'paper', 'scissors','spock','lizard'];
 
   var n = Math.floor(Math.random() * 11);
 
-  while (n > 2) {
+  while (n > 4) {
     n = Math.floor(Math.random() * 11);
   }
 
@@ -308,10 +533,14 @@ function showHouse() {
 };
 
 
-function won(div) {
+function wonpaper(div) {
 
-  if (div === ".paperhouse") {
-    return "0"
+  if (div === ".lizardhouse") {
+    $(".dothouse1").delay(3000).fadeIn();
+    $(".dothouse2").delay(3000).fadeIn();
+    $(".dothouse3").delay(3000).fadeIn();
+
+    return "-1"
 
   } else if (div === ".rockhouse") {
     $(".dotyou1").delay(3000).fadeIn();
@@ -321,7 +550,7 @@ function won(div) {
     return "1";
 
 
-  } else {
+  } else if(div === ".scissorshouse") {
   $(".dothouse1").delay(3000).fadeIn();
   $(".dothouse2").delay(3000).fadeIn();
   $(".dothouse3").delay(3000).fadeIn();
@@ -329,13 +558,29 @@ function won(div) {
   return "-1"
 
   }
+  else if(div === ".spockhouse")
+  {
+    $(".dotyou1").delay(3000).fadeIn();
+    $(".dotyou2").delay(3000).fadeIn();
+    $(".dotyou3").delay(3000).fadeIn();
+
+    return "1";
+  }
+
+  else{
+    return 0;
+  }
 }
 
 function wonsicssors(div)
 {
-  if(div === ".scissorshouse")
+  if(div === ".lizardhouse")
   {
-    return 0;
+    $(".dotyou1").delay(3000).fadeIn();
+  $(".dotyou2").delay(3000).fadeIn();
+  $(".dotyou3").delay(3000).fadeIn();
+
+  return "1";
   }
 
   else if(div ===".paperhouse"){
@@ -347,7 +592,7 @@ function wonsicssors(div)
   return "1";
 
 
-} else {
+} else if(div ===".rockhouse") {
 $(".dothouse1").delay(3000).fadeIn();
 $(".dothouse2").delay(3000).fadeIn();
 $(".dothouse3").delay(3000).fadeIn();
@@ -355,13 +600,29 @@ $(".dothouse3").delay(3000).fadeIn();
 return "-1"
 
 }
+else if(div === ".spockhouse")
+{
+  $(".dothouse1").delay(3000).fadeIn();
+  $(".dothouse2").delay(3000).fadeIn();
+  $(".dothouse3").delay(3000).fadeIn();
+
+  return "-1"
+
+}
+else{
+  return 0;
+}
 }
 
 function wonrock(div)
 {
-  if(div === ".rockhouse")
+  if(div === ".lizardhouse")
   {
-    return 0;
+    $(".dotyou1").delay(3000).fadeIn();
+    $(".dotyou2").delay(3000).fadeIn();
+    $(".dotyou3").delay(3000).fadeIn();
+
+    return "1";
   }
 
   else if(div ===".scissorshouse"){
@@ -373,12 +634,104 @@ function wonrock(div)
   return "1";
 
 
-} else {
+} else if (div ===".spockhouse") {
 $(".dothouse1").delay(3000).fadeIn();
 $(".dothouse2").delay(3000).fadeIn();
 $(".dothouse3").delay(3000).fadeIn();
 
 return "-1"
 
+}
+else if(div === ".paperhouse")
+{
+  $(".dothouse1").delay(3000).fadeIn();
+  $(".dothouse2").delay(3000).fadeIn();
+  $(".dothouse3").delay(3000).fadeIn();
+
+  return "-1"
+}
+else{
+  return 0;
+}
+}
+
+function wonspock(div)
+{
+  if(div === ".rockhouse")
+  {
+    $(".dotyou1").delay(3000).fadeIn();
+    $(".dotyou2").delay(3000).fadeIn();
+    $(".dotyou3").delay(3000).fadeIn();
+
+    return "1";
+  }
+
+  else if(div ===".scissorshouse"){
+
+  $(".dotyou1").delay(3000).fadeIn();
+  $(".dotyou2").delay(3000).fadeIn();
+  $(".dotyou3").delay(3000).fadeIn();
+
+  return "1";
+
+
+} else if(div ===".paperhouse" ) {
+  $(".dothouse1").delay(3000).fadeIn();
+  $(".dothouse2").delay(3000).fadeIn();
+  $(".dothouse3").delay(3000).fadeIn();
+
+  return "-1"
+
+}
+else if(div === ".lizardhouse")
+{
+  $(".dothouse1").delay(3000).fadeIn();
+  $(".dothouse2").delay(3000).fadeIn();
+  $(".dothouse3").delay(3000).fadeIn();
+  return "-1";
+}
+else {
+  return 0;
+}
+}
+
+function wonlizard(div)
+{
+  if(div === ".rockhouse")
+  {
+    $(".dothouse1").delay(3000).fadeIn();
+    $(".dothouse2").delay(3000).fadeIn();
+    $(".dothouse3").delay(3000).fadeIn();
+
+    return "-1"
+  }
+
+  else if(div ===".scissorshouse"){
+
+    $(".dothouse1").delay(3000).fadeIn();
+    $(".dothouse2").delay(3000).fadeIn();
+    $(".dothouse3").delay(3000).fadeIn();
+
+    return "-1"
+
+
+} else if(div ===".paperhouse" ) {
+  $(".dotyou1").delay(3000).fadeIn();
+  $(".dotyou2").delay(3000).fadeIn();
+  $(".dotyou3").delay(3000).fadeIn();
+
+  return "1";
+
+}
+else if(div ===".spockhouse")
+{
+  $(".dotyou1").delay(3000).fadeIn();
+  $(".dotyou2").delay(3000).fadeIn();
+  $(".dotyou3").delay(3000).fadeIn();
+
+  return "1";
+}
+else{
+  return 0;
 }
 }
